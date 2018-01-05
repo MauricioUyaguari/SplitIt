9545626924dcc07ab4265a4a5d731716f5879219
Mauricio Uyaguari <lmauricio12@gmail.com>
2018-01-05 18:58:17 -0500

    styling modal continue :

--- frontend/components/friends/friends_create_modal.jsx
+++ frontend/components/friends/friends_create_modal.jsx
@@ -6,28 +6,10 @@ import Modal from 'react-modal';
 
 const customStyles = {
   overlay : {
-    position          : 'fixed',
-    top               : 0,
-    left              : 0,
-    right             : 0,
-    bottom            : 0,
-    backgroundColor   : 'rgba(255, 255, 255, 0.75)'
+    position          : 'fixed'
   },
   content : {
     position                   : 'absolute',
-    top                        : '40px',
-    left                       : '35%',
-    right                      : '35%',
-    width                      : '30%',
-    bottom                     : '40px',
-    border                     : '1px solid #999',
-    background                 : '#fff',
-    overflow                   : 'auto',
-    WebkitOverflowScrolling    : 'touch',
-    borderRadius               : '15px',
-    outline                    : 'none',
-    padding                    : '0px'
-
   }
 };
 
@@ -57,15 +39,19 @@ class FriendCreateModal extends React.Component {
             onRequestOpen={this.closeModal}
             style={customStyles}
             ariaHideApp={false}
+            className={{
+            base: 'add-friend-modal',
+            afterOpen: 'add-friend-modal',
+            beforeClose: 'add-friend-modal'
+          }}
             >
-            <div className="add-friend-modal">
               <h2 className="add-friend-header">
                 <div>Add a Friend</div>
-                <div>X</div>
+                <div onClick={this.closeModal} >X</div>
               </h2>
               <FriendFormContainer />
-              <button className="close-add-friend" onClick={this.closeModal}>Close</button>
-            </div>
+              <button className="close-add-friend" onClick={this.closeModal}>Cancel</button>
+              <div></div>
           </Modal>
       </div>
       );
