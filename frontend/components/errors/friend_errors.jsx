83629b108ab7f999476c672044cf0264435ffb91
Mauricio Uyaguari <lmauricio12@gmail.com>
2018-02-14 23:12:41 -0500

    adding friend errors render

---
+++ frontend/components/errors/friend_errors.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const FriendErrors = (props) => {
+
+
+    return(
+      <div>
+        This is where erros will go if there are any
+      </div>
+    );
+};
+
+export default FriendErrors;
